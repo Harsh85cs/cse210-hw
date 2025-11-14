69813aa4ee83e0b90637b565e1e781e37eba4cc5
Harsh85cs <Abrahamolopezp@gmail.com>
2025-11-14 02:48:05 -0600

    finish

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your score?");
+        string scoreInput = Console.ReadLine();
+        int score = int.Parse(scoreInput);
+
+        if (score >= 90)
+        {
+            if (score  >= 97)
+            {
+                Console.WriteLine("Your grade is A+.");
+            }
+            else if (score >= 93)
+            {
+                Console.WriteLine("Your grade is A-.");
+            }
+            else
+            {
+                Console.WriteLine("Your grade is A.");
+            }
+        }
+        else if (score >= 80)
+        {
+            Console.WriteLine("Your grade is B.");
+        }
+        else if (score >= 70)
+        {
+            Console.WriteLine("Your grade is C.");
+        }
+        else if (score >= 60)
+        {
+            Console.WriteLine("Your grade is D.");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F.");
+        }
+
     }
 }
