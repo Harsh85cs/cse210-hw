2909d03634f1862632b2610bf5fcdb1363226402
Harsh85cs <Abrahamolopezp@gmail.com>
2025-11-22 18:17:06 -0600

    fix

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -10,17 +10,17 @@ class Program
         // Add multiple scriptures
         scriptures.Add(new Scripture(
             new Reference("Proverbs", 3, 5, 6),
-            "Trust in the Lord with all thine heart and lean not unto thine own understanding."
+            "Trust in the Lord with all thine hear"
         ));
 
         scriptures.Add(new Scripture(
             new Reference("John", 3, 16),
-            "For God so loved the world that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."
+            "For God so loved the world that he gave his only begotten Son"
         ));
 
         scriptures.Add(new Scripture(
             new Reference("2 Nephi", 2, 25),
-            "Adam fell that men might be; and men are, that they might have joy."
+            "Adam fell that men might be; and men are, "
         ));
 
         // Pick one at random
